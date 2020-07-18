d8b109f2cbf3f20094fdbf50493c3ecdb33c2e0e
Matin <mra9776@gmail.com>
2020-07-18 09:05:35 +0430

    Now it's going to check for of all the free officers, when one got free.

--- src/main/java/com/github/mra9776/saving_privates_bike/Matcher.java
+++ src/main/java/com/github/mra9776/saving_privates_bike/Matcher.java
@@ -42,6 +42,20 @@ public class Matcher {
 				cases.setOfficer_id(officer.getOfficerId());
 				cases.setCaseStatus(CaseStatus.WORKING);
 				casesRepository.save(cases);
+				
+				// Let's make Other Officers busy.
+				// but there's a problem 
+				// the way I'm gonna handle this problem
+				// - Is kindda make a chain of calles till one of our resources finishes, and might create 
+				//   some over head. but It's one of simplest solutions.
+				// - The other problem is checking a known variable (count of available resource) I could solve it 
+				//   Using an Atomic variable, although this can save us some processing time, 
+				//	 but I have faith on cache of Database to optimize out these extra queries, 
+				//	 And of course concurrency might be a pain in the ash too.
+				
+				if (0 < Math.min(availableCases.size() -1 , availableOfficers.size() -1 )){
+					assign();
+				}
 			}
 		}
 	}
